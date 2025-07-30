74b92ca0e182db68d844646656c5a2fb8bc79de4
Muhamad Ikhsan <146111335+Jo2205@users.noreply.github.com>
2025-07-31 01:33:35 +0700

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
 
-echo "[runtime] Mengaktifkan virtual environment..."
-source /opt/venv/bin/activate
-
-echo "[runtime] Menjalankan aplikasi..."
-exec gunicorn -w 1 -b 0.0.0.0:8080 app:app
